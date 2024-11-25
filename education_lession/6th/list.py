4399bc24389cbdfe8af762215fbd46794aba341c
Egor Cherenkov <egor.cherenkov@dcsportsoft.com>
2024-11-25 11:14:03 +0300

    Education slice 4th

--- education_lession/6th/list.py
+++ education_lession/6th/list.py
@@ -22,11 +22,58 @@
 #
 # for item in shop_list:
 #     print(item)
+#
+# menu = ['продукты', 'акции', 'товары']
+# print("Меню:")
+#
+# for item, value in enumerate(menu, start=1):
+#     print(f'{item}. {value}')
 
-menu = ['продукты', 'акции', 'товары']
-print("Меню:")
+# Картавая задачка №2 с циклом for
 
-for item, value in enumerate(menu, start=1):
-    print(f'{item}. {value}')
+# BAD_LETTER = 'ж'
+# THRESHOLD = 2
+#
+# user_words = input('Я проанализирую введённые слова и верну список с результатам\n'
+#                    'Введите слова через пробел: ').lower().split()
+#
+# result = []
+#
+# for word in user_words:
+#     bed_letter_count = word.count(BAD_LETTER)
+#     if bed_letter_count >= THRESHOLD:
+#         result.append(word)
+#
+# if result:
+#     print(f'Слова с буквой {BAD_LETTER} в количестве больше {THRESHOLD} или равно: {result}')
+# else:
+#     print('Таких слов нет')
+
+# Создание списка продуктов с помощью for и range
+
+# num_product = int(input('Введите количество продуктов: '))
+# shop_list = []
+#
+# for i in range(num_product):
+#     product = input(f'Введите название продукта {i + 1}: ').lower()
+#     shop_list.append(product)
+#
+# print(f'Ваш список продуктов {list(enumerate(shop_list, start=1))}')
+
+# Реализуйте функцию, которая возвращает самое часто встречаемое слово в слайсе (массиве).
+# Если таких слов несколько, то возвращается первое из них
+
+word_list = ["hello", "world", "my", "name", "is", "world", "hello", "my", "hello"]
+
+for word in word_list:
+    count = word_list.count(word)
+    if count > 1:
+        print(f'Слово {word} встречается {count} раза')
+    else: break
+
+
+
+
+
 
 
